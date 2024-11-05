747241f68bdb04eddf077592057f80ad9b18610d
LamanAliyeva14 <lamania@code.edu.az>
2024-11-05 13:53:56 +0400

    ADDED TASK

---
+++ Static-Extension-CustomTypeSort/Static Extention Methods.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Static_Extension_CustomTypeSort
+{
+	public static class Static_Extention_Methods
+	{
+		public static bool Isodd(this int a)
+		{
+			return a % 2 == 1;
+		}
+
+		public static bool isEven(this int a)
+		{
+			return a % 2 == 0;
+		}
+
+		public static bool isContainsDigit(this string a)
+		{
+			foreach (char c in a)
+			{
+				if (char.IsDigit(c))
+				{
+					return true;
+				}
+			}
+            return false;
+        }
+
+		public static string ToCapitalize(this string a)
+		{
+			return char.ToUpper(a[0]) + a.Substring(1).ToLower();
+		}
+
+		public static int[] GetVaueIndexes(this string word, char b)
+		{
+			int[] ints = new int[0];
+			for (int i=0; i<word.Length; i++)
+			{
+				 if (word[i] == b)
+				 {
+					Array.Resize(ref ints, ints.Length + 1);
+					ints[ints.Length - 1] = i;
+				 }
+			}
+			return ints;
+		}
+
+		public static string GetFirstSentence(this string word)
+		{
+			int index = 0;
+			for (int i=0; i<word.Length; i++)
+			{
+				if (word[i]=='.')
+				{
+					index = i;
+					break;
+				}
+			}
+			return word.Substring(0, index);
+		}
+
+		
+	}
+}
+
---
+++ Static-Extension-CustomTypeSort/Student.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace Static_Extension_CustomTypeSort
+{
+    public class Student
+    {
+        private string _fullName;
+        private string _groupNo;
+        private byte _age;
+
+        public string GroupNo
+        {
+            get => _groupNo;
+            set
+            {
+                if (CorrectGroupNo(value))
+                {
+                    _groupNo = value;
+                }
+                else
+                    Console.WriteLine("GroupNo is incorrect");
+            }
+        }
+
+        private string ValidName(string name, string surname)
+        {
+            if (string.IsNullOrWhiteSpace(name) || (string.IsNullOrWhiteSpace(surname)))
+            {
+                return "Name or Surname must be added";
+            }
+            if (CorrectFullName(name) && CorrectFullName(surname))
+            {
+                return Capitalize(name) + " " + Capitalize(surname);
+            }
+            return "Name or Surname is incorrect";
+
+
+
+        }
+
+        // GroupNo memberi ardicil olaraq bir boyuk verf ve 3 reqemli bir natural ededed ibaret olmalidir
+        private static bool CorrectGroupNo(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value) || value.Length != 4)
+                return false;
+            return (char.IsUpper(value[0]) && char.IsDigit(value[1]) && char.IsDigit(value[2]) && char.IsDigit(value[3]));
+
+        }
+
+        // Fullname memberi ad ve soyadi ifade edir,ona gore de fullname value-u ad ve soyaddan ibaret olmalidir.Formati ad +bosluq+soyad seklinde olmalidir
+
+        private static bool CorrectFullName(string input)
+        {
+            foreach (char c in input)
+                if (char.IsLetter(c))
+                {
+                    return true;
+                }
+            return false;
+        }
+
+        private static string Capitalize(string input)
+        {
+            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+        }
+
+
+
+    }
+}
+
+
